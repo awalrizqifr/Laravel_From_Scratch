2efb73865e868e798cf693e0e5f5997325d5d3f4
awalrizqifr <awalrizqifr@gmail.com>
2021-03-07 00:24:22 +0700

    Meet and Use Controllers in Laravel to Handle the Business Logic

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return 'This is the list of products FROM CONTROLLER';
+    }
+
+    public function create()
+    {
+        return 'A form to create a product FROM CONTROLLER';
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($product)
+    {
+        return "Showing product {$product} FROM CONTROLLER";
+    }
+
+    public function edit($product)
+    {
+        return "Showing the form to edit the product {$product} FROM CONTROLLER";
+    }
+
+    public function update($product)
+    {
+        //
+    }
+
+    public function destroy($product)
+    {
+        //
+    }
+}
