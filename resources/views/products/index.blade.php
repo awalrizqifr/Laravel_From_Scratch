9d2ef78ffab2d152c85dc12e8d1c202a2cd81c77
awalrizqifr <awalrizqifr@gmail.com>
2021-03-07 22:22:34 +0700

    Reviewing Some Features to Improve the Usability of the Laravel Project

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,9 +2,10 @@
 
 @section('content')
     <h1>List of Products</h1>
+    <a class="btn btn-success" href="{{ route('products.create') }}">Create</a>
     @empty ($products)
     <div class="alert alert-warning">
-        The list of ptoducts is empty
+        The list of products is empty
     </div>
     @else
     <div class="table-responsive">
@@ -16,6 +17,7 @@
                 <th>Price</th>
                 <th>Stock</th>
                 <th>Status</th>
+                <th>Actions</th>
             </thead>
             <tbody>
                 @foreach ($products as $product)
@@ -26,6 +28,10 @@
                         <td>{{ $product->price }}</td>
                         <td>{{ $product->stock }}</td>
                         <td>{{ $product->status }}</td>
+                        <td>
+                            <a class="btn btn-link" href="{{ route('products.show', ['product' => $product->id]) }}">Show</a>
+                            <a class="btn btn-link" href="{{ route('products.edit', ['product' => $product->id]) }}">Edit</a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
