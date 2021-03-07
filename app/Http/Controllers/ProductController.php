c86b8e54c73878c84cf63d834eaf36985dee4d7f
awalrizqifr <awalrizqifr@gmail.com>
2021-03-07 21:39:47 +0700

    Learn About the Essential Control Structures in Blade

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,9 +11,10 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();        
-        dd($products);
 
-        return view('products.index');
+        return view('products.index')->with([
+            'products' => $products,
+        ]);
     }
 
     public function create()
