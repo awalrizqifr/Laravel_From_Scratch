7f2334320d60db1e27fb8009fcf3b5dfb1645174
awalrizqifr <awalrizqifr@gmail.com>
2021-03-10 15:47:29 +0700

    Meet and Use the One-to-Many Polymorphic Relationships in Laravel

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -31,4 +31,9 @@ class Product extends Model
     {
         return $this->belongsToMany(Order::class)->withPivot('quantity');
     }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
