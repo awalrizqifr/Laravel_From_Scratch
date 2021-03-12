9330e25d8d991bb2de34b830cf1845827c0722c8
awalrizqifr <awalrizqifr@gmail.com>
2021-03-12 13:10:54 +0700

    Implementing the Main Admin Panel Action with Laravel

--- routes/panel.php
+++ routes/panel.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Panel\PanelController;
 use App\Http\Controllers\Panel\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,7 @@ use Illuminate\Support\Facades\Route;
 | Admin Panel Routes
 |--------------------------------------------------------------------------
 */
+
+Route::get('panel', [PanelController::class, 'index'])->name('panel');
 
 Route::resource('products', ProductController::class);
