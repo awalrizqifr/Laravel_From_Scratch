11304533a0fda194d1d8dbb146560d9dcebe002e
awalrizqifr <awalrizqifr@gmail.com>
2021-03-13 13:46:24 +0700

    Editing a User's Profile with Laravel

--- app/Http/Controllers/profileController.php
+++ app/Http/Controllers/profileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProfileRequest;
 use Illuminate\Http\Request;
 
 class profileController extends Controller
@@ -18,8 +19,21 @@ class profileController extends Controller
         ]);
     }
 
-    public function update()
+    public function update(ProfileRequest $request)
     {
-        //
+        $user = $request->user();
+
+        $user->fill($request->validated());
+
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
+            $user->sendEmailVerificationNotification();
+        }
+
+        $user->save();
+
+        return redirect()
+            ->route('profile.edit')
+            ->withSuccess('Profile updated');
     }
 }
---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule as ValidationRule;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                ValidationRule::unique('users')->ignore($this->user()->id)
+            ],
+            'password' => ['nullable', 'string', 'min:8', 'confirmed'],
+            'image' => ['nullable', 'image'],
+        ];
+    }
+}
