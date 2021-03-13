880c6ed0f7c9907bc0193291848ddea7b7809ca9
awalrizqifr <awalrizqifr@gmail.com>
2021-03-13 09:03:42 +0700

    Use Database Transactions in Laravel for Complex Operations

--- app/Http/Controllers/OrderPaymentController.php
+++ app/Http/Controllers/OrderPaymentController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use App\Models\Payment;
 use App\Services\CartService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderPaymentController extends Controller
 {
@@ -38,21 +39,23 @@ class OrderPaymentController extends Controller
      */
     public function store(Request $request, Order $order)
     {
-        //PaymentService::handlePayment()
+        return DB::transaction(function () use($order) {
+            //PaymentService::handlePayment()
 
-        $this->cartService->getFromCookie()->products()->detach();
+            $this->cartService->getFromCookie()->products()->detach();
 
-        $order->payment()->create([
-            'amount' => $order->total,
-            'payed_at' => now(),
-        ]);
+            $order->payment()->create([
+                'amount' => $order->total,
+                'payed_at' => now(),
+            ]);
 
-        $order->status = 'payed';
+            $order->status = 'payed';
 
-        $order->save();
+            $order->save();
 
-        return redirect()
-            ->route('main')
-            ->withSuccess("Thanks! We received your \${$order->total} payment.");
+            return redirect()
+                ->route('main')
+                ->withSuccess("Thanks! We received your \${$order->total} payment.");
+        });
     }
 }
